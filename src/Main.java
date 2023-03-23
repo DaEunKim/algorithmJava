d3e568252b5bba803e2363a5661e13d1b9d309d1
kimdaeun <de.kim@wconcept.co.kr>
2023-03-23 18:25:16 +0900

    sample code

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * packageName    :
+ * fileName       : ${NAME}
+ * author         : ${USER}
+ * date           : ${DATE}
+ * description    :
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        helloworld s = new helloworld();
+        //입력요소를 선언해줘야 출력값이 나옴
+        int[] a = {5, 9, 7, 10};
+        s.helloworld(a, 5);
+    }
+}
